7881a8a7094def20c9bd281653986b16492a2e3a
Irina <apkarianirina@gmail.com>
2017-09-21 20:07:04 +0300

    Start draw face and the text

--- Nutella.py
+++ Nutella.py
@@ -7,6 +7,9 @@ def main(a):
     t.color('black')
     t.pensize(2)
     pot(a)
+    eyes(a)
+    mouth(a)
+    nut_text(a)
 
 
 def pot(a):
@@ -111,6 +114,58 @@ def cap(a):
         t.right(90)
 
 
-main(200)
+def eyes(a):
+    t.home()
+    t.penup()
+    t.right(90)
+    t.forward(a * 0.1875)
+    t.left(90)
+    t.forward(a / 4)
+    apple(a)
+    t.forward(-a / 2)
+    apple(a)
+
+
+def apple(a):
+    t.pendown()
+    t.circle(a / 10)
+    t.penup()
+
+
+def mouth(a):
+    t.home()
+    t.penup()
+    t.right(90)
+    t.forward(a * 0.1875)
+    t.left(90)
+    t.pendown()
+
+    t.begin_fill()
+    # t.fillcolor('red')
+    t.forward(a / 10)
+    t.right(90)
+    t.forward(a / 16)
+    t.circle(-a / 10, 180)
+    t.forward(a / 16)
+    t.right(90)
+    t.forward(a / 10)
+    t.end_fill()
+
+    # t.penup()
+
+
+def nut_text(a):
+    t.home()
+    t.penup()
+    t.forward(-a / 2)
+    t.left(90)
+    t.pendown()
+    # t.forward(a / 10)
+    # t.write('n')
+    t.color('red')
+    t.write('Nutella', font=('Arial', 72))
+
+
+main(300)
 
 t.getscreen().mainloop()
